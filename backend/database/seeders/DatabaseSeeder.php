ebedef5736f9c63d450d206db9e828818f6ba6b0
koch-attila <koch.attila.git@gmail.com>
2025-09-10 17:11:25 +0200

    DatabaseSeeder updated

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -19,5 +19,13 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $this->call([
+            UserSeeder::class,
+            CategorySeeder::class,
+            ProductSeeder::class,
+            RatingSeeder::class,
+            OrderSeeder::class,
+        ]);
     }
 }
