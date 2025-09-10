b3b4d7091fe7e0312d9be6ca88959b2b8ea17a63
koch-attila <koch.attila.git@gmail.com>
2025-09-10 17:33:56 +0200

    API created

---
+++ backend/routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RatingController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+Route::apiResource('products', ProductController::class);
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('orders', OrderController::class);
+Route::apiResource('ratings', RatingController::class);
+
+Route::get('/products/{product}/ratings', [ProductController::class, 'ratings']);
+Route::get('/products/{product}/categories', [ProductController::class, 'categories']);
+Route::get('/categories/{category}/products', [CategoryController::class, 'products']);
+Route::get('/ratings/{rating}/users', [RatingController::class, 'user']);
