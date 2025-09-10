6ed56cd9a422f286d91b5f1818da2af168832165
koch-attila <koch.attila.git@gmail.com>
2025-09-10 16:53:13 +0200

    Created migrations

---
+++ backend/database/migrations/2025_08_10_144834_create_ratings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('title')->nullable();
+            $table->text('comment')->nullable();
+            $table->unsignedTinyInteger('rating');
+            $table->timestamps();
+
+            $table->unique(['product_id', 'user_id']);
+            $table->index('rating');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ratings');
+    }
+};
